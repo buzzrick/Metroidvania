8fac6ca91a1166a5c0dbbc13ec17861f3fb8f5d4
Beric Holt <beric@runawayplay.com>
2022-09-07 13:40:01 +1200

    Added simple interactable door trigger and animation.

---
+++ Assets/Metroidvania/Interactables/WorldObjects/ObjectSwitchable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Metroidvania.Interactables.WorldObjects
+{
+    public class ObjectSwitchable : MonoBehaviour
+    {
+        public Animator SwitchAnimator;
+        public bool DefaultToggleState = false;
+
+        private readonly int SwitchHash = Animator.StringToHash("Switch");
+
+        private void Awake()
+        {
+            SetSwitchState(DefaultToggleState);   
+        }
+
+        public void ToggleSwitchState()
+        {
+            SetSwitchState(!GetSwitchState());
+        }
+
+        public bool GetSwitchState()
+        {
+            return SwitchAnimator.GetBool(SwitchHash);
+        }
+
+        public void SetSwitchState(bool state)
+        {
+            SwitchAnimator.SetBool(SwitchHash, state);
+        }
+    }
+}
---
+++ Assets/Metroidvania/Interactables/WorldObjects/ObjectSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Metroidvania.Interactables.WorldObjects
+{
+
+    [RequireComponent(typeof(Collider))]
+    public class ObjectSwitcher : MonoBehaviour, IPlayerInteractable
+    {
+        public ObjectSwitchable[] ObjectsToSwitch;
+
+        public async UniTask<bool> InteractAsync()
+        {
+            if (ObjectsToSwitch.Length == 0)
+                return false;   
+
+            foreach (var switcher in ObjectsToSwitch)
+            {
+                switcher.ToggleSwitchState();
+            }
+
+            return true;
+        }
+    }
+}
