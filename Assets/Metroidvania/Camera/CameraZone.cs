02a282ebc31e09940003fd1ea5a36cdea3b6ea3e
Beric Holt <beric@runawayplay.com>
2022-09-24 15:05:10 +1200

    Start of dynamic lighting switching within scenes.

--- Assets/Metroidvania/Camera/CameraZone.cs
+++ Assets/Metroidvania/Camera/CameraZone.cs
@@ -1,6 +1,7 @@
 using Metroidvania.Interactables;
 using Metroidvania.Player;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace Metroidvania.Camera
@@ -9,6 +10,7 @@ namespace Metroidvania.Camera
     public class CameraZone : MonoBehaviour, IPlayerEnterTriggerZone, IPlayerExitTriggerZone
     {
         public string CameraName;
+        public string ActiveLightingScene;
 
         private CameraController _cameraController;
 
@@ -21,11 +23,26 @@ namespace Metroidvania.Camera
         public void OnPlayerEnteredZone(PlayerRoot player)
         {
             _cameraController.SetPrioritisedCamera(this);
+
+            //  switch the active scene. This causes the current LightingSettings to change
+            if (!string.IsNullOrEmpty(ActiveLightingScene))
+            {
+                SceneManager.SetActiveScene(SceneManager.GetSceneByName(ActiveLightingScene));
+            }
+            else
+            {
+                SetDefaultScene();
+            }
         }
 
         public void OnPlayerExitedZone(PlayerRoot player)
         {
             _cameraController.ClearPrioritisedCamera(this);
+        }
+
+        private void SetDefaultScene()
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("GameCoreScene"));
         }
     }
 }
