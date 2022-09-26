49a8577b7b38b17da6518a460de0b050a1303df0
Beric Holt <beric@runawayplay.com>
2022-09-26 17:11:14 +1300

    Moved some debugging scripts to correct folder.

---
+++ Assets/Metroidvania/Debugging/ListButton.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Metroidvania.Debugging
+{
+    [RequireComponent(typeof(Button))]
+    public class ListButton : MonoBehaviour
+    {
+        public TMPro.TextMeshProUGUI ButtonText;
+        private Button _button;
+
+        public event Action<string> OnButtonClick;
+
+        public void SetButtonText(string text)
+        {
+            _button = GetComponent<Button>();
+            ButtonText.text = text;
+            _button.onClick.AddListener(HandleButtonClick);
+        }
+
+        private void HandleButtonClick()
+        {
+            OnButtonClick?.Invoke(ButtonText.text);
+        }
+    }
+}
