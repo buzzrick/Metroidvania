4b73af404c3e4fd42b8c7ee59b5ff35053cd9673
Beric Holt <beric@runawayplay.com>
2023-07-09 13:42:39 +1200

    Rudimentary Action animation setup.

---
+++ Assets/Metroidvania/Player/Animation/PlayerAnimationActionsView.cs
@@ -0,0 +1,81 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+
+
+namespace Metroidvania.Player.Animation
+{
+    public class PlayerAnimationActionsView
+    {
+        private PlayerAnimationView _playerAnimationView;
+        private Animator _animator;
+        private int _actionLayerID;
+        public readonly int HashActionInteract = Animator.StringToHash("ActionInteract");
+        public readonly int HashActionChopDiagonal = Animator.StringToHash("ActionChopDiagonal");
+        public readonly int HashActionMining = Animator.StringToHash("ActionMining");
+
+        private float _timer = 0f;
+
+        public PlayerAnimationActionsView(PlayerAnimationView playerAnimationView, Animator animator)
+        {
+            _playerAnimationView = playerAnimationView;
+            _animator = animator;
+            _actionLayerID = _animator.GetLayerIndex("ActionLayer");
+
+            GetRightHandBone();
+        }
+
+        private void GetRightHandBone()
+        {
+            foreach (var bone in _animator.avatar.humanDescription.human)
+            {
+                if (bone.humanName == "RightHand")
+                {
+                    Debug.Log(bone.boneName);
+                }
+            }
+        }
+
+        public void StartActionAnimation(int actionID)
+        {
+            _animator.SetTrigger(actionID);
+            SetToolForAnimation(actionID);
+
+            FadeInOutLayerWeight(2);
+        }
+
+        private void SetToolForAnimation(int actionID)
+        {
+            if (actionID == HashActionChopDiagonal)
+            {
+
+            }
+        }
+
+        private async void FadeInOutLayerWeight(float seconds, float maxWeight = 1f)
+        {
+            _timer = seconds;
+            _animator.SetLayerWeight(_actionLayerID, maxWeight);
+            await UniTask.WaitUntil(()=>!IsActionAnimationRunning());
+            _animator.SetLayerWeight(_actionLayerID, 0);
+        }
+
+        public void Tick()
+        {
+            if (_timer > 0f)
+            {
+                _timer -= Time.deltaTime;
+            }
+        }
+
+        public bool IsActionAnimationRunning()
+        {
+            return _timer > 0f;
+        }
+
+        public void Reset()
+        {
+            _animator.SetLayerWeight(_actionLayerID, 0);
+        }
+    }
+}
--- Assets/Metroidvania/Player/Animation/PlayerAnimationView.cs
+++ Assets/Metroidvania/Player/Animation/PlayerAnimationView.cs
@@ -1,110 +1,115 @@
-using Metroidvania.Player;
-using Metroidvania.Player.Animation;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerAnimationView : MonoBehaviour, ICharacterAnimationView
+namespace Metroidvania.Player.Animation
 {
-    private ICharacterMovementDriver _characterMovementDriver;
-    [SerializeField] private PlayerInteractionController _playerInteractionController;
-    private Animator _animator;
-    private Transform _root;
-    private Transform _projector;
-    [SerializeField] private GameObject _model;
-    private Transform _headBone;
-    private Rigidbody[] _boneRig;
-    private readonly int HashSpeed = Animator.StringToHash("Speed");
-    private readonly int HashJump = Animator.StringToHash("Jump");
-    private readonly int HashGrounded = Animator.StringToHash("Grounded");
-    private readonly int HashInteraction = Animator.StringToHash("Interact");
-    private float mass = 0.1f;	// Mass of each bone
-
-    private void Awake()
+    public class PlayerAnimationView : MonoBehaviour, ICharacterAnimationView
     {
-        _animator = gameObject.GetComponent<Animator>();
-        if (_characterMovementDriver == null)
+        private ICharacterMovementDriver _characterMovementDriver;
+        [SerializeField] private PlayerInteractionController _playerInteractionController;
+        [SerializeField] private GameObject _model;
+        private Animator _animator;
+        private PlayerAnimationActionsView _playerAnimationActionView;
+        private Transform _root;
+        private Transform _headBone;
+        private Rigidbody[] _boneRig;
+        private int _actionLayerID;
+        private readonly int HashSpeed = Animator.StringToHash("Speed");
+        private readonly int HashJump = Animator.StringToHash("Jump");
+        private readonly int HashGrounded = Animator.StringToHash("Grounded");
+        private float mass = 0.1f;  // Mass of each bone
+
+        private void Awake()
         {
-            _characterMovementDriver = GetComponentInParent<ICharacterMovementDriver>();
-        }
-        _characterMovementDriver.RegisterCharacterAnimationView(this);
-
-        _playerInteractionController.OnInteraction += HandleOnInteractionStarted;
-        _playerInteractionController.OnInteractionFailed += HandleOnInterationFailed;
-
-        if (_root == null)
-            _root = transform.Find("Root");
-        if (_model == null)
-        {
-            var modelObject = transform.Find("MicroMale") ?? transform.Find("MicroFemale");
-            _model = modelObject.gameObject;
-        }
-        if (_headBone == null)
-            _headBone = transform.Find("Head");
-        _boneRig = gameObject.GetComponentsInChildren<Rigidbody>();
-        DisableRagdoll();
-    }
-
-    private void HandleOnInterationFailed()
-    {
-    }
-
-    private void HandleOnInteractionStarted()
-    {
-        _animator.SetTrigger(HashInteraction);
-    }
-
-    public void SetSpeed(float speed)
-    {
-        _animator.SetFloat(HashSpeed, speed);
-    }
-
-    public void SetJumping(bool isJumping)
-    {
-        //_animator.SetBool(HashJump, isJumping);
-    }
-
-    public void SetGrounded(bool isGrounded)
-    {
-        _animator.SetBool(HashGrounded, isGrounded);
-    }
-
-
-    public void DisableRagdoll()
-    {
-        foreach (Component ragdoll in _boneRig)
-        {
-            if ((ragdoll.GetComponent<Collider>() != null) && ragdoll.GetComponent<Collider>() != this.GetComponent<Collider>())
+            _animator = gameObject.GetComponent<Animator>();
+            if (_characterMovementDriver == null)
             {
-                ragdoll.GetComponent<Collider>().enabled = false;
-                ragdoll.GetComponent<Rigidbody>().isKinematic = true;
-                ragdoll.GetComponent<Rigidbody>().mass = 0.01f;
+                _characterMovementDriver = GetComponentInParent<ICharacterMovementDriver>();
             }
-        }
-        GetComponent<Collider>().enabled = true;
-    }
+            _characterMovementDriver.RegisterCharacterAnimationView(this);
 
-    public IEnumerator EnableRagdoll(float delay, Vector3 force)
-    {
-        yield return new WaitForSeconds(delay);
-        foreach (Component ragdoll in _boneRig)
+            _playerInteractionController.OnInteraction += HandleOnInteractionStarted;
+            _playerInteractionController.OnInteractionFailed += HandleOnInterationFailed;
+
+            if (_root == null)
+                _root = transform.Find("Root");
+            if (_model == null)
+            {
+                var modelObject = transform.Find("MicroMale") ?? transform.Find("MicroFemale");
+                _model = modelObject.gameObject;
+            }
+            if (_headBone == null)
+                _headBone = transform.Find("Head");
+            _boneRig = gameObject.GetComponentsInChildren<Rigidbody>();
+
+            DisableRagdoll();
+            _playerAnimationActionView = new PlayerAnimationActionsView(this, _animator);
+            _playerAnimationActionView.Reset();
+        }
+
+        private void Update()
         {
-            if (ragdoll.GetComponent<Collider>() != null)
-                ragdoll.GetComponent<Collider>().enabled = true;
-            ragdoll.GetComponent<Rigidbody>().isKinematic = false;
-            ragdoll.GetComponent<Rigidbody>().mass = mass;
-            if (force.magnitude > 0)
-                ragdoll.GetComponent<Rigidbody>().AddForce(force * UnityEngine.Random.value);
+            _playerAnimationActionView.Tick();   
         }
-        GetComponent<Animator>().enabled = false;
-        GetComponent<Collider>().enabled = false;
 
-        //Destroy(GetComponent<BotControlScript>());
-        
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<Rigidbody>().useGravity = false;
+        private void HandleOnInterationFailed()
+        {
+        }
+
+        private void HandleOnInteractionStarted()
+        {
+            _playerAnimationActionView.StartActionAnimation(_playerAnimationActionView.HashActionChopDiagonal);
+        }
+
+        public void SetSpeed(float speed)
+        {
+            _animator.SetFloat(HashSpeed, speed);
+        }
+
+        public void SetJumping(bool isJumping)
+        {
+            //_animator.SetBool(HashJump, isJumping);
+        }
+
+        public void SetGrounded(bool isGrounded)
+        {
+            _animator.SetBool(HashGrounded, isGrounded);
+        }
+
+
+        public void DisableRagdoll()
+        {
+            foreach (Component ragdoll in _boneRig)
+            {
+                if ((ragdoll.GetComponent<Collider>() != null) && ragdoll.GetComponent<Collider>() != this.GetComponent<Collider>())
+                {
+                    ragdoll.GetComponent<Collider>().enabled = false;
+                    ragdoll.GetComponent<Rigidbody>().isKinematic = true;
+                    ragdoll.GetComponent<Rigidbody>().mass = 0.01f;
+                }
+            }
+            GetComponent<Collider>().enabled = true;
+        }
+
+        public IEnumerator EnableRagdoll(float delay, Vector3 force)
+        {
+            yield return new WaitForSeconds(delay);
+            foreach (Component ragdoll in _boneRig)
+            {
+                if (ragdoll.GetComponent<Collider>() != null)
+                    ragdoll.GetComponent<Collider>().enabled = true;
+                ragdoll.GetComponent<Rigidbody>().isKinematic = false;
+                ragdoll.GetComponent<Rigidbody>().mass = mass;
+                if (force.magnitude > 0)
+                    ragdoll.GetComponent<Rigidbody>().AddForce(force * UnityEngine.Random.value);
+            }
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+
+            //Destroy(GetComponent<BotControlScript>());
+
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().useGravity = false;
+        }
     }
-
-
-
 }
-
