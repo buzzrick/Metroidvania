aee4c8af79c1165586776dbaca16dcb34a51fe22
Beric Holt <beric@runawayplay.com>
2024-02-07 09:09:32 +1300

    FreeWorldUnlocksDebugging no longer persists between runs.

--- Assets/Metroidvania/Configuration/GameConfiguration.cs
+++ Assets/Metroidvania/Configuration/GameConfiguration.cs
@@ -11,15 +11,11 @@ namespace Metroidvania.Configuration
         public bool DebugOptionsEnabled => _debugOptionsEnabled;
 
         [SerializeField, EnableIf("DebugOptionsEnabled")] private bool _freeWorldUnlocks;
-        public bool FreeWorldUnlocks => DebugOptionsEnabled && _freeWorldUnlocks;
+        public bool FreeWorldUnlocks => DebugOptionsEnabled &&  (_freeWorldUnlocks || FreeWorldUnlocksDebugging);
         
         /// <summary>
-        /// Allows us to modify free world unlocks for debugging at runtime
+        /// Allows us to modify free world unlocks for debugging at runtime (resets every time the game starts
         /// </summary>
-        public bool FreeWorldUnlocksDebugging 
-        { 
-            get { return _freeWorldUnlocks;}
-            set { _freeWorldUnlocks = value;}
-        }
+        public bool FreeWorldUnlocksDebugging { get; set; } 
     }
 }
