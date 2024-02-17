05511ad7497dfb905928539ca8a0fbb3640dc782
Beric Holt <beric@runawayplay.com>
2024-02-17 17:26:15 +1300

    Added Editor Button to WorldUnlockNodes to automatically generate new child nodes and wire them up.

--- Assets/Metroidvania/World/WorldUnlockNodeBase.cs
+++ Assets/Metroidvania/World/WorldUnlockNodeBase.cs
@@ -1,6 +1,7 @@
 #nullable enable
-using System;
 using Buzzrick.UnityLibs.Attributes;
+using NaughtyAttributes;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Metroidvania.World
@@ -111,5 +112,69 @@ namespace Metroidvania.World
                 Gizmos.DrawLine(transform.position, node.transform.position);
             }
         }
+
+
+
+#if UNITY_EDITOR
+
+        private const string PrefabPath = "Assets/Metroidvania/World/WorldUnlockNodePrefab.prefab";
+
+        [Button]
+        private void GenerateChildNode()
+        {
+            string newName = GenerateNextNodeName(name);
+
+            WorldUnlockNode nodePrefab = UnityEditor.AssetDatabase.LoadAssetAtPath<WorldUnlockNode>(PrefabPath);
+            WorldUnlockNode newObject = Instantiate(nodePrefab, transform.position + Vector3.forward, Quaternion.identity, transform);
+            newObject.name = newName;
+            newObject.transform.SetParent(transform.parent);
+
+            GameObject childObjects = new GameObject($"{newName}Objects");
+            childObjects.transform.SetParent(newObject.transform);
+            childObjects.transform.position = newObject.transform.position;
+            childObjects.transform.SetParent(transform.parent);
+
+            newObject.NodeObjects = new GameObject[] { childObjects };
+
+            WorldUnlockNode[] newChildNodes = new WorldUnlockNode[ChildNodes.Length + 1];
+            for (int i = 0; i < ChildNodes.Length; i++)
+            {
+                newChildNodes[i] = ChildNodes[i];
+            }
+            newChildNodes[ChildNodes.Length] = newObject;
+            ChildNodes = newChildNodes;
+
+            //newObject.transform.localPosition = transform.position + Vector3.forward;
+            //newObject.AddComponent<BoxCollider>();
+
+            Debug.Log($"Create Node: {newName}");
+        }
+
+        private static Regex LastCharacters = new Regex("\\d+$");
+        private string GenerateNextNodeName(string nodeName)
+        {
+            string numericPart = LastCharacters.Match(nodeName).Groups[0].Value;
+            string newName;
+            string nonNumericPart = nodeName;
+            if (int.TryParse(numericPart, out int number))
+            {
+                nonNumericPart = nodeName.Substring(0, nodeName.Length - numericPart.Length);
+            }
+            while (true)
+            {
+                number++;
+                newName = $"{nonNumericPart}{number}";
+                // se if we can find an object with the same name, and if so increment the number and try again
+                var foundObject = GameObject.Find(newName);
+                if (foundObject == null)
+                {
+                    break;
+                }
+            }
+
+            return newName;
+        }
+#endif
+
     }
 }
