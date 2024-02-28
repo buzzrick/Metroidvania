bcaefba47be5810d6538c1377aa7faece1ce20d4
Beric Holt <beric@runawayplay.com>
2024-02-28 15:18:44 +1300

    Added Version text display.

---
+++ Assets/Metroidvania/UI/VersionText.cs
@@ -0,0 +1,22 @@
+﻿using Buzzrick.UnityLibs.Attributes;
+using TMPro;
+using UnityEngine;
+
+namespace Metroidvania.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class VersionText : MonoBehaviour
+    {
+        [SerializeField, RequiredField] private TMP_Text _textField = default!;
+
+        private void Start()
+        {
+            _textField.text = Application.version;
+        }
+
+        private void Reset()
+        {
+            _textField = GetComponent<TMP_Text>();
+        }
+    }
+}
